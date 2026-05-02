b38c4790de171680ad0bc433f137daf4197c8512
KAjalshahi123 <shahikajal274508@gmail.com>
2026-05-02 20:55:39 +0530

    add2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,17 +1,34 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:8080/api", // your backend base URL
+// });
+
+// // ✅ ADD INTERCEPTOR HERE
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api", // your backend base URL
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// ✅ ADD INTERCEPTOR HERE
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
